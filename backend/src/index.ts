a3599d042ef84a914766d3b8be04ce0a4e9da5db
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-17 16:33:52 -0300

    chore :package: configurando o container corretamente

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
