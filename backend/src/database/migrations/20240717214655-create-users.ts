4c8607ffaa2f65246bd181346d245aeeb18dab75
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-17 18:58:43 -0300

    chore :package: configurando cli e sequelize

---
+++ backend/src/database/migrations/20240717214655-create-users.ts
@@ -0,0 +1,44 @@
+'use strict';
+
+import { QueryInterface, DataTypes } from 'sequelize';
+
+module.exports = {
+  up: async (queryInterface: QueryInterface) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      fullname: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      username: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface: QueryInterface) => {
+    await queryInterface.dropTable('users');
+  },
+};
