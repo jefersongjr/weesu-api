e26ded134ded13c9b93d78e1aa22ceb0c849ee63
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-18 16:07:26 -0300

    feat :sparkles: criando service de login para validar login com token

--- backend/src/services/UserService.ts
+++ backend/src/services/UserService.ts
@@ -1,6 +1,7 @@
 import User from '../database/models/UserModel';
 import { tokenGenerate } from '../utils/tokenGenerate';
 import ThrowException from '../middlewares/exceptions/ThrowException';
+import { Jwt } from 'jsonwebtoken';
 // import * as bcrypt from 'bcrypt';
 
 export class UserService {
@@ -21,5 +22,12 @@ export class UserService {
     const { name } = user;
     const token = tokenGenerate.makeToken(name, email);
     return token;
+  };
+
+  public getLoginValidate = async (token: string | undefined) => {
+    if (!token) throw new ThrowException(401, 'Token invalid');
+    const user = await tokenGenerate.validateToken(token);
+    console.log(user);
+    return user as Jwt;
   };
 }
