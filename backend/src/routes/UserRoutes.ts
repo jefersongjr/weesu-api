f43b963e0f80a9fc110ed8a82c50cd6fae4b782b
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-18 15:48:35 -0300

    feat :sparkles: criando rota de login para gerar token

--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -5,6 +5,5 @@ const userRouter = Router();
 const userController = new UserController();
 
 userRouter.post('/login', userController.getLogin);
-userRouter.get('/users', userController.getAllUsers);
 
 export { userRouter };
