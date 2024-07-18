06af59274e9d00c98a2bb6596b9c830f993cbb4b
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-17 22:31:43 -0300

    fix :bug: corrigindo erro de typo

--- backend/src/database/migrations/20240717214655-create-users.ts
+++ backend/src/database/migrations/20240717214655-create-users.ts
@@ -1,17 +1,15 @@
 'use strict';
 
-import { QueryInterface, DataTypes } from 'sequelize';
-
 module.exports = {
-  up: async (queryInterface: QueryInterface) => {
-    await queryInterface.createTable('users', {
+  up: async (QueryInterface: any , DataTypes: any) => {
+    await QueryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: DataTypes.INTEGER,
       },
-      name: {
+      fullname: {
         allowNull: false,
         type: DataTypes.STRING,
       },
@@ -35,7 +33,7 @@ module.exports = {
     });
   },
 
-  down: async (queryInterface: QueryInterface) => {
-    await queryInterface.dropTable('users');
+  down: async (QueryInterface: any) => {
+    await QueryInterface.dropTable('users');
   },
 };
