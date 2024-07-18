98f53685a5eaf117f983aea5b18d104a67ecb45d
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-18 02:19:57 -0300

    refactor :recycle: comentando seeders e migrations

--- backend/src/database/seeders/20240717221423-users-seeder.ts
+++ backend/src/database/seeders/20240717221423-users-seeder.ts
@@ -1,29 +1,29 @@
-'use strict';
-module.exports = {
-  up: async (QueryInterface) => {
-    await QueryInterface.bulkInsert('users', [
-      {
-        fullname: 'James Happer',
-        password:
-          '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
-        // senha: secret_admin
-        email: 'jim_happer@ds.com',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        fullname: 'Dwight Schrute',
-        password:
-          '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO',
-        // senha: secret_user
-        email: 'ds_manager@dm.com',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]);
-  },
+// 'use strict';
+// module.exports = {
+//   up: async (QueryInterface) => {
+//     await QueryInterface.bulkInsert('users', [
+//       {
+//         fullname: 'James Happer',
+//         password:
+//           '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+//         // senha: secret_admin
+//         email: 'jim_happer@ds.com',
+//         createdAt: new Date(),
+//         updatedAt: new Date(),
+//       },
+//       {
+//         fullname: 'Dwight Schrute',
+//         password:
+//           '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO',
+//         // senha: secret_user
+//         email: 'ds_manager@dm.com',
+//         createdAt: new Date(),
+//         updatedAt: new Date(),
+//       },
+//     ]);
+//   },
 
-  down: async (QueryInterface) => {
-    await QueryInterface.bulkDelete('users', {}, {});
-  },
-};
+//   down: async (QueryInterface) => {
+//     await QueryInterface.bulkDelete('users', {}, {});
+//   },
+// };
