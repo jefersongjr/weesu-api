7318b8f34f6853fd663b4bbd9b7bdb9d34e56345
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-18 16:08:45 -0300

    feat :sparkles: criando rota para validar  login com token

--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/UserController';
+import { authMiddleware } from '../middlewares/authMiddleware';
 
 const userRouter = Router();
 const userController = new UserController();
 
 userRouter.post('/login', userController.getLogin);
+userRouter.get('/login/validate', authMiddleware, userController.validateLogin);
 
 export { userRouter };
