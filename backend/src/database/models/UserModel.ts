051b038de37b2fb0922ad459a5bd161cd28a5363
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-18 12:26:23 -0300

    feat :sparkles: criando model User

---
+++ backend/src/database/models/UserModel.ts
@@ -0,0 +1,40 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  declare id: number;
+  declare name: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: STRING,
+      allowNull: false,
+    },
+    email: {
+      type: STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    underscored: true,
+    modelName: 'users',
+    timestamps: false,
+  },
+);
+export default User;
