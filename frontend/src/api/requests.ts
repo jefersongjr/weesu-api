c74143d7c4b000d2907f597996d3f5c9d6e727e9
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-18 23:45:06 -0300

    feat :sparkles: criando request para fazer fazer login com dados do banco de dados

--- frontend/src/api/requests.ts
+++ frontend/src/api/requests.ts
@@ -9,4 +9,16 @@ interface LoginRequest {
   password: string;
 }
 
+export const requestLogin = async (
+  urlApi: string,
+  objectLogin: LoginRequest,
+): Promise<AxiosResponse | void> => {
+  try {
+    const response = await api.post(urlApi, objectLogin);
+    return response;
+  } catch (error) {
+    console.error('Erro ao fazer login:', error);
+  }
+};
+
 export default api;
