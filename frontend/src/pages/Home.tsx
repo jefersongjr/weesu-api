77f998b82967f0b1bb0a955b9c91a9dd353b2551
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-19 00:28:39 -0300

    feat :sparkles: validando o token na home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,25 @@
-function Home() {
+import { Box, Typography } from '@mui/material';
+import React, { useEffect } from 'react';
+import { validateToken } from '../api/requests';
+import { useNavigate } from 'react-router-dom';
+const Home: React.FC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token === null) {
+      navigate('/login');
+      return;
+    }
+    const tokenIsValid = validateToken(token);
+    if (!tokenIsValid) navigate('/login');
+  }, []);
+
   return (
-    <div>
-      <h1>Seja Bem Vindo</h1>
-    </div>
+    <Box sx={{ textAlign: 'center', mt: 4 }}>
+      <Typography variant="h2">Bem Vindo!</Typography>
+    </Box>
   );
-}
+};
 
 export default Home;
