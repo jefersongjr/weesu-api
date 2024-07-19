cd5c3c0142ecfb56d01950d2b6f36e783c9a23bf
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-18 23:44:01 -0300

    feat :sparkles: adicionando configuração de cors para o backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,21 +1,30 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { userRouter } from './routes/UserRoutes';
+import cors from 'cors';
 
 class App {
   public app: express.Express;
 
   constructor() {
     this.app = express();
+    this.config();
     this.app.use(bodyParser.json());
 
     this.app.use(userRouter);
 
-    this.config();
     this.routes();
   }
 
   private config(): void {
+    this.app.use(
+      cors({
+        origin: 'http://localhost:5173',
+        methods: 'GET,POST,PUT,DELETE',
+        allowedHeaders: 'Content-Type,Authorization',
+      }),
+    );
+
     this.app.use(express.json());
   }
 
