899add07ace6107b0483eeabfe04c20292dee2a1
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-19 14:12:04 -0300

    refactor : recycle: comentando os seeders

--- backend/src/database/migrations/20240719153352-create-products.ts
+++ backend/src/database/migrations/20240719153352-create-products.ts
@@ -1,71 +1,71 @@
-'use strict';
+// 'use strict';
 
-/**
- * @type {import('sequelize-cli').Migration}
- */
+// /**
+//  * @type {import('sequelize-cli').Migration}
+//  */
 
-module.exports = {
-  up: async (QueryInterface, DataTypes) => {
-    await QueryInterface.createTable('products', {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      description: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-      },
-      price: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false,
-      },
-      quantity: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      model: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      referencia: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      brand: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      image_url: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'users',
-          key: 'id',
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-      },
-      createdAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      updatedAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-    });
-  },
+// module.exports = {
+//   up: async (QueryInterface, DataTypes) => {
+//     await QueryInterface.createTable('products', {
+//       id: {
+//         type: DataTypes.INTEGER,
+//         autoIncrement: true,
+//         primaryKey: true,
+//         allowNull: false,
+//       },
+//       name: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//       },
+//       description: {
+//         type: DataTypes.TEXT,
+//         allowNull: true,
+//       },
+//       price: {
+//         type: DataTypes.DECIMAL(10, 2),
+//         allowNull: false,
+//       },
+//       quantity: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//       },
+//       model: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//       },
+//       referencia: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//       },
+//       brand: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//       },
+//       image_url: {
+//         type: DataTypes.STRING,
+//         allowNull: true,
+//       },
+//       user_id: {
+//         type: DataTypes.INTEGER,
+//         references: {
+//           model: 'users',
+//           key: 'id',
+//         },
+//         onUpdate: 'CASCADE',
+//         onDelete: 'SET NULL',
+//       },
+//       createdAt: {
+//         type: DataTypes.DATE,
+//         allowNull: false,
+//       },
+//       updatedAt: {
+//         type: DataTypes.DATE,
+//         allowNull: false,
+//       },
+//     });
+//   },
 
-  async down(QueryInterface) {
-    await QueryInterface.dropTable('products');
-  },
-};
+//   async down(QueryInterface) {
+//     await QueryInterface.dropTable('products');
+//   },
+// };
