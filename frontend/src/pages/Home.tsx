c2b750a0b78b273541428815bcc27b4e04f3adb8
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-20 17:39:00 -0300

    refactor :recycle: renderizando todos os produtos

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -61,7 +61,11 @@ const Home: React.FC = () => {
           Lista de Produtos
         </Typography>
         <Grid container justifyContent="center">
-          <ProductCard product={products} />
+          {products.map((product) => (
+            <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+              <ProductCard product={product} />
+            </Grid>
+          ))}
         </Grid>
         <Button
           variant="contained"
