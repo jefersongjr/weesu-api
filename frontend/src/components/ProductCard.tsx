f58907e488f104f5b3f5d3d3cac761c3c319d0c7
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-20 17:38:43 -0300

    refactor :recycle: editando o Card

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -4,35 +4,36 @@ import { ProductCardProps } from '../interfaces';
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   return (
-    <Card sx={{ maxWidth: 345, margin: 2 }}>
+    <Card sx={{ maxWidth: 345, margin: 2, height: '480px' }}>
       <CardMedia
         component="img"
         height="220"
-        image={product[0]?.image_url}
-        alt={product[0]?.name}
+        image={product?.image_url}
+        alt={product?.name}
+        sx={{ objectFit: 'contain' }}
       />
-    <CardContent>
+      <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {product[0]?.name}
+          {product?.name}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          {product[0]?.description}
+          {product?.description}
         </Typography>
         <Box sx={{ mt: 2 }}>
           <Typography variant="body2" color="text.primary">
-            <strong>Price:</strong> ${product[0]?.price}
+            <strong>Preço de venda:</strong> ${product?.price}
           </Typography>
           <Typography variant="body2" color="text.primary">
-            <strong>Quantity:</strong> {product[0]?.quantity}
+            <strong>Quantidade em estoque:</strong> {product?.quantity}
           </Typography>
           <Typography variant="body2" color="text.primary">
-            <strong>Model:</strong> {product[0]?.model}
+            <strong>Modelo:</strong> {product?.model}
           </Typography>
           <Typography variant="body2" color="text.primary">
-            <strong>Reference:</strong> {product[0]?.referencia}
+            <strong>Referência:</strong> {product?.referencia}
           </Typography>
           <Typography variant="body2" color="text.primary">
-            <strong>Brand:</strong> {product[0]?.brand}
+            <strong>Marca:</strong> {product?.brand}
           </Typography>
         </Box>
       </CardContent>
