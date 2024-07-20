f48ef174b1262e1037c2a2259c86c6d16e72b73b
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-20 17:09:45 -0300

    feat :sparkles: criando interfaces

---
+++ frontend/src/interfaces/index.ts
@@ -0,0 +1,16 @@
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface Product {
+  name: string;
+  description: string;
+  price: number;
+  quantity: number;
+  model: string;
+  referencia: string;
+  brand: string;
+  image_url: string;
+  user_id: number;
+}
