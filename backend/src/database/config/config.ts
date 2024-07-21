973fb2eea28ee29060f7a35b33042bb53816790e
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-21 04:07:04 -0300

    chore :package: configurando arquivo de deploy

--- backend/src/database/config/config.ts
+++ backend/src/database/config/config.ts
@@ -2,11 +2,11 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT),
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT),
   dialect: 'postgres',
   logging: false,
 };
