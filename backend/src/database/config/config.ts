aba87829da0eacbcdbaa08af5baadf457aa874aa
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-21 08:00:12 -0300

    fix :bug: configurando deploy

--- backend/src/database/config/config.ts
+++ backend/src/database/config/config.ts
@@ -2,9 +2,9 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
   host: process.env.PGHOST,
   port: Number(process.env.PGPORT),
   dialect: 'postgres',
