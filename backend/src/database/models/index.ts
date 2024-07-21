c1549fda913df885820d7f88ff0d85b2b6c643ef
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-21 07:48:42 -0300

    fix :bug: configurando deploy

--- backend/src/database/models/index.ts
+++ backend/src/database/models/index.ts
@@ -1,7 +1,18 @@
 import config from '../config/config';
 import { Sequelize } from 'sequelize';
+import pg from 'pg';
 
-const sequelize = new Sequelize(config);
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if (!DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined in environment variables.');
+}
+
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialect: 'postgres',
+  dialectModule: pg,
+  ...config,
+});
 
 sequelize
   .authenticate()
