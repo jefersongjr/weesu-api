92ce743d4a7ae6e18e6891530c810aa2c42b7b5e
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-21 00:45:08 -0300

    feat :sparkles: criando modal de confirmação de exclusão

---
+++ frontend/src/components/Modal.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Typography,
+} from '@mui/material';
+import { deleteProduct } from '../api/requests';
+
+interface ConfirmationModalProps {
+  open: boolean;
+  handleClose: () => void;
+  id: number;
+  onSuccess: () => void;
+}
+
+const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
+  open,
+  handleClose,
+  id,
+  onSuccess,
+}) => {
+  const handleDelete = async () => {
+    try {
+      await deleteProduct('/products', id);
+      onSuccess();
+      handleClose();
+    } catch (error) {
+      console.error('Erro ao deletar produto:', error);
+    }
+  };
+
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>Confirmação de Exclusão</DialogTitle>
+      <DialogContent>
+        <Typography variant="body1">
+          Tem certeza que deseja apagar o produto?
+        </Typography>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">
+          Cancelar
+        </Button>
+        <Button onClick={handleDelete} color="error">
+          Apagar
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ConfirmationModal;
