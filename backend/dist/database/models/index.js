9a2181cf3e94bba904b998cf9d2e127a80802d55
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-21 06:03:07 -0300

    chore :package:  testando deploy

--- backend/dist/database/models/index.js
+++ backend/dist/database/models/index.js
@@ -1,9 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = require("dotenv");
 const sequelize_1 = require("sequelize");
 const sequelize = new sequelize_1.Sequelize('database', 'username', 'password', {
     host: 'localhost',
     dialect: 'postgres',
+    ...dotenv_1.config,
 });
 exports.default = sequelize;
 //# sourceMappingURL=index.js.map
--- backend/src/database/models/index.ts
+++ backend/src/database/models/index.ts
@@ -1,8 +1,10 @@
+import { config } from 'dotenv';
 import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize('database', 'username', 'password', {
   host: 'localhost',
   dialect: 'postgres',
+  ...config,
 });
 
 export default sequelize;
