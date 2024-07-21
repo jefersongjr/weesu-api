15ec13395a8e97ed8dffaa6795ca73e08ac49484
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-21 06:18:29 -0300

    chore :package:  testando deploy

--- backend/src/database/models/index.ts
+++ backend/src/database/models/index.ts
@@ -1,9 +1,11 @@
 import { config } from 'dotenv';
+import * as pg from 'pg';
 import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize('database', 'username', 'password', {
   host: 'localhost',
   dialect: 'postgres',
+  dialectModule: pg,
   ...config,
 });
 
