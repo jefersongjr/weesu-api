af1ba27f283cba37df42e20d16e044efa146723c
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-21 01:45:52 -0300

    feat :sparkles: criando url para editar produto

--- frontend/src/components/HomeContent.tsx
+++ frontend/src/components/HomeContent.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import CreateProductForm from './CreateProductForm';
 import ListProducts from './ListProducts';
+import EditProductForm from './EditProductForm';
 
 const HomeContent: React.FC = () => {
   return (
     <Routes>
       <Route path="create-product" element={<CreateProductForm />} />
+      <Route path="edit-product" element={<EditProductForm />} />
       <Route path="product-list" element={<ListProducts />} />
     </Routes>
   );
