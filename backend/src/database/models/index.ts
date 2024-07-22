4ba6284bf86f3f815a127302167ffffc78aced13
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-21 23:30:56 -0300

    tentando corrigir deploy

--- backend/src/database/models/index.ts
+++ backend/src/database/models/index.ts
@@ -1,9 +1,30 @@
-import * as pg from 'pg';
 import { Sequelize } from 'sequelize';
+import pg from 'pg';
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize('postgres://admin:admin@localhost:5432/mydb', {
-  dialect: 'postgres',
-  dialectModule: pg,
-});
+dotenv.config();
+
+const { DATABASE_URL, PGUSER, PGPASSWORD, PGDATABASE, PGHOST, PGPORT } =
+  process.env;
+
+if (!PGDATABASE || !PGUSER || !PGPASSWORD || !PGHOST || !PGPORT) {
+  throw new Error('Required environment variables are not defined');
+}
+
+const isProduction = !!DATABASE_URL;
+
+const sequelize = isProduction
+  ? new Sequelize(DATABASE_URL!, {
+      dialect: 'postgres',
+      dialectModule: pg,
+      logging: false,
+    })
+  : new Sequelize(PGDATABASE!, PGUSER!, PGPASSWORD!, {
+      host: PGHOST,
+      port: Number(PGPORT) || 5432,
+      dialect: 'postgres',
+      dialectModule: pg,
+      logging: false,
+    });
 
 export default sequelize;
