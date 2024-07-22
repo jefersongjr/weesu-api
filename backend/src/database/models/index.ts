706b7585d2eff4cf1e2a165f819721adecc3fda5
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-21 23:24:59 -0300

    tentando corrigir deploy

--- backend/src/database/models/index.ts
+++ backend/src/database/models/index.ts
@@ -1,26 +1,9 @@
+import * as pg from 'pg';
 import { Sequelize } from 'sequelize';
-import pg from 'pg';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-const { DATABASE_URL, PGUSER, PGPASSWORD, PGDATABASE, PGHOST, PGPORT } =
-  process.env;
-
-const isProduction = !!DATABASE_URL;
-
-const sequelize = isProduction
-  ? new Sequelize(DATABASE_URL, {
-      dialect: 'postgres',
-      dialectModule: pg,
-      logging: false,
-    })
-  : new Sequelize(PGDATABASE!, PGUSER!, PGPASSWORD, {
-      host: PGHOST,
-      port: Number(PGPORT),
-      dialect: 'postgres',
-      dialectModule: pg,
-      logging: false,
-    });
+const sequelize = new Sequelize('postgres://admin:admin@localhost:5432/mydb', {
+  dialect: 'postgres',
+  dialectModule: pg,
+});
 
 export default sequelize;
