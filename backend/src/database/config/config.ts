c9a9c771f844f341bdfbf84d19961a743dbb1437
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-21 22:13:43 -0300

    refactor :recycle: mudando index.ts

--- backend/src/database/config/config.ts
+++ backend/src/database/config/config.ts
@@ -2,11 +2,11 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'password',
+  username: process.env.PGUSER || 'root',
+  password: process.env.PGPASSWORD || 'password',
   database: 'weesu_db',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 5432,
+  host: process.env.PGHOST || 'localhost',
+  port: Number(process.env.PGPORT) || 5432,
   dialect: 'postgres',
   dialectOptions: {
     timezone: 'Z',
