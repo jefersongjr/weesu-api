0cd218311a5fffba8ca0bc90c2ea3479f3cf86c3
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-21 21:54:58 -0300

    refactor :recycles: mundando options

--- backend/src/database/config/config.ts
+++ backend/src/database/config/config.ts
@@ -5,7 +5,7 @@ const config: Options = {
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || 'password',
   database: 'weesu_db',
-  host: process.env.DB_HOST || 'db',
+  host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 5432,
   dialect: 'postgres',
   dialectOptions: {
