c4d5b796dc730f05cb33d6984e74ee5796ccf747
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-22 15:44:42 -0300

    habilitando cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -20,6 +20,15 @@ class App {
       }),
     );
 
+    // Configurar CSP
+    this.app.use((req, res, next) => {
+      res.setHeader(
+        'Content-Security-Policy',
+        "default-src 'self'; img-src 'self' https://weesu-api.onrender.com; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'",
+      );
+      next();
+    });
+
     this.app.use(userRouter);
     this.app.use(productRouter);
 
