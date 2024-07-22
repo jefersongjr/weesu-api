87fd1e2e01074306b1d1219ca0da9e4ff10b1268
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-21 22:03:19 -0300

    refactor :recycle: mudando index.ts

--- backend/src/database/models/index.ts
+++ backend/src/database/models/index.ts
@@ -1,26 +1,4 @@
 import { Sequelize } from 'sequelize';
-import pg from 'pg';
-import dotenv from 'dotenv';
+import * as config from '../config/config';
 
-dotenv.config();
-
-const { DATABASE_URL, PGUSER, PGPASSWORD, PGDATABASE, PGHOST, PGPORT } =
-  process.env;
-
-const isProduction = !!DATABASE_URL;
-
-const sequelize = isProduction
-  ? new Sequelize(DATABASE_URL, {
-      dialect: 'postgres',
-      dialectModule: pg,
-      logging: false,
-    })
-  : new Sequelize(PGDATABASE!, PGUSER!, PGPASSWORD, {
-      host: PGHOST,
-      port: Number(PGPORT),
-      dialect: 'postgres',
-      dialectModule: pg,
-      logging: false,
-    });
-
-export default sequelize;
+export default new Sequelize(config);
